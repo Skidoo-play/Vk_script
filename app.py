029c8f20d20d7d84fe92b12dce53004648e2efe6
Михаил <pugumo68@gmail.com>
2019-06-26 21:37:36 +0700

    Set routing

--- app.py
+++ app.py
@@ -1,15 +1,18 @@
-from flask import Flask, jsonify, request
+from flask import Flask
+from flask import jsonify
+from flask import request
 from remoteFacade import AccountFacade
 
 app = Flask(__name__)
 
 
 @app.route('/user')
-def hello_world():
+def user():
     user_ids = request.args.get('user_ids', default=None, type=str)
     return jsonify(AccountFacade.get_account(user_ids))
 
-@app.route('/f')
+
+@app.route('/user/friends')
 def f():
     user_ids = request.args.get('user_ids', default=None, type=str)
     return jsonify(AccountFacade.get_friends(user_ids))
