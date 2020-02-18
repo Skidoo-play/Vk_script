e519bac2bf2dd4879717f97b078b2ee019fb130b
Mikhail Uljakov <pugumo68@gmail.com>
2020-02-18 19:59:50 +0700

    Split tests data from test code

---
+++ test_data/data.py
@@ -0,0 +1,14 @@
+
+class User:
+    URL = "/api/user?user_ids=shzfrnia"
+    RESULT = {
+        "avatar": "https://sun9-68.userapi.com/c856032/v856032090/108f51/ObW5ybDwiZ8.jpg?ava=1",
+        "days_offline": None,
+        "first_name": "Mikhail",
+        "id": 135480774,
+        "is_deactivated": None,
+        "is_online": False,
+        "last_name": "Ulyakov",
+        "last_seen": None,
+        "link": "https://vk.com/id135480774"
+    }
