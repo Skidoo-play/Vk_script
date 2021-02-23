7c39d37dcd336444dede10d3a9ba07522ba3292c
Mikhail Uljakov <pugumo68@gmail.com>
2021-02-23 14:20:27 +0700

    Refactoring

--- src/remoteFacade.py
+++ src/remoteFacade.py
@@ -2,52 +2,51 @@ from .accountMapper import AccountMapper
 from .assemblers import AccountAssembler, FriendsAssembler
 
 
-class AccountFacade: #TODO добавить метод для проверки существования аккаунта
-    @staticmethod
-    def __get_account(user_ids):
+class AccountFacade:  # TODO добавить метод для проверки существования аккаунта
+    @classmethod
+    def __get_account(cls, user_ids):
         """Return account object"""
         return AccountMapper.get_user(user_ids)
 
-    @staticmethod
-    def __serealize_account(account_object):
+    @classmethod
+    def __serealize_account(cls, account_object):
         """Return JSON string of account"""
         return AccountAssembler.serialize(account_object)
 
-    @staticmethod
-    def get_account(user_ids):
+    @classmethod
+    def get_account(cls, user_ids):
         """"Return JSON STRING"""
-        account_vk = AccountFacade.__get_account(user_ids)
-        account_vk_JSON = AccountFacade.__serealize_account(account_vk)
-        return account_vk_JSON
+        return cls.__serealize_account(cls.__get_account(user_ids))
 
-    @staticmethod
-    def get_friends(user_ids):
+    @classmethod
+    def get_friends(cls, user_ids):
         """Return JSON string"""
-        account_vk = AccountFacade.__get_account(user_ids)
-        friends_list = AccountMapper.get_public_friends(account_vk)
-        friends_list_JSON = FriendsAssembler.serealize(friends_list)
-        return friends_list_JSON
+        friends_list = AccountMapper.get_public_friends(
+            cls.__get_account(user_ids)
+        )
+        return FriendsAssembler.serealize(friends_list)
 
-    @staticmethod
-    def get_banned_friends(user_ids):
+    @classmethod
+    def get_banned_friends(cls, user_ids):
         """Return JSON string"""
-        account_vk = AccountFacade.__get_account(user_ids)
-        banned_friends = AccountMapper.get_banned_friends(account_vk)
-        banned_friends_JSON = FriendsAssembler.serealize(banned_friends)
-        return banned_friends_JSON
+        banned_friends = AccountMapper.get_banned_friends(
+            cls.__get_account(user_ids)
+        )
+        return FriendsAssembler.serealize(banned_friends)
 
-    @staticmethod
-    def get_deleted_friends(user_ids):
+    @classmethod
+    def get_deleted_friends(cls, user_ids):
         """Return JSON string"""
-        account_vk = AccountFacade.__get_account(user_ids)
-        deleted_friends = AccountMapper.get_deleted_friends(account_vk)
-        deleted_friends_JSON = FriendsAssembler.serealize(deleted_friends)
-        return deleted_friends_JSON
+        deleted_friends = AccountMapper.get_deleted_friends(
+            cls.__get_account(user_ids)
+        )
+        return FriendsAssembler.serealize(deleted_friends)
 
-    @staticmethod
-    def get_abandoned_friends(user_ids, days_offline):
+    @classmethod
+    def get_abandoned_friends(cls, user_ids, days_offline):
         """Return JSON string"""
-        account_vk = AccountFacade.__get_account(user_ids)
-        abandoned_friends = AccountMapper.get_non_active_friends(account_vk, days_offline=days_offline)
-        abandoned_friends_JSON = FriendsAssembler.serealize(abandoned_friends)
-        return abandoned_friends_JSON
+        abandoned_friends = AccountMapper.get_non_active_friends(
+            cls.__get_account(user_ids),
+            days_offline=days_offline
+        )
+        return FriendsAssembler.serealize(abandoned_friends)
